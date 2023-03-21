45be100ce727a2e1bb1fa1195a2e7d58a833df7d
Lee Cooper <lee.cooper@northwestern.edu>
2023-03-20 21:15:29 -0500

    Update __init__.py

--- glimr/__init__.py
+++ glimr/__init__.py
@@ -3,16 +3,16 @@ This package contains hyperparameter tuning tools
 """
 
 # make functions available at the package level using shadow imports
+from glimr.keras import (
+    keras_losses,
+    keras_metrics,
+    keras_optimizer
+)
 from glimr.optimization import optimization_space
 from glimr.search import Search
 from glimr.utils import (
     check_tunable,
-    keras_losses,
-    keras_metrics,
-    keras_optimizer,
     set_hyperparameter,
-    string_to_loss,
-    string_to_metric,
 )
 
 # list out things that are available for public use
