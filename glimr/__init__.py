4624ee592f5bfaf980e5553242c3c9491a39248c
Lee Cooper <cooperle@gmail.com>
2023-03-22 11:09:37 -0500

    replace glimr notation with ray.tune.search

--- glimr/__init__.py
+++ glimr/__init__.py
@@ -3,28 +3,18 @@ This package contains hyperparameter tuning tools
 """
 
 # make functions available at the package level using shadow imports
-from glimr.keras import (
-    keras_losses,
-    keras_metrics,
-    keras_optimizer
-)
+from glimr.keras import keras_losses, keras_metrics, keras_optimizer
 from glimr.optimization import optimization_space
 from glimr.search import Search
-from glimr.utils import (
-    check_tunable,
-    set_hyperparameter,
-)
+from glimr.utils import sample_space
 
 # list out things that are available for public use
 __all__ = (
     # functions and classes of this package
-    "check_tunable",
     "keras_losses",
     "keras_metrics",
     "keras_optimizer",
     "optimization_space",
-    "set_hyperparameter",
-    "string_to_loss",
-    "string_to_metric",
+    "sample_space",
     "Search",
 )
--- glimr/utils.py
+++ glimr/utils.py
@@ -1,100 +1,19 @@
-from ray import tune
-import tensorflow as tf
+from ray.tune.search import sample
 
 
-def check_tunable(value, expected_type, arg_name):
-    """A function for checking the values provided to define tunable objects.
+def sample_space(space):
+    """Sample a configuration from a Ray Tune search space.
 
-    This function is used by configuration-generating functions to ensure that the values defining
-    the range or categories used in search are valid. For sets and lists, each element is checked.
-
-    Parameters
-    ----------
-    value : int, float, str or list of int, float, str, or set of int, float, str
-        The value to check.
-    expected_type : type
-        The expected type. For sets or lists, we check that all elements are of this type.
-    arg_name : str
-        Name of argument in configuration. Used for producing informative error messages.
+    This function recurses through a nested dictionary defining a search space, sampling
+    a value when a search space hyperparameter is encountered.
     """
 
-    type_name = expected_type.__name__
-    if not isinstance(value, expected_type):
-        # set can be artibrary number of any item type, so long as consistent
-        if isinstance(value, set):
-            for v in value:
-                if not isinstance(v, expected_type):
-                    raise ValueError(
-                        f"{arg_name} set expects elements of type {type_name}, received {type(v).__name__}"
-                    )
-
-        # list can be a 2- or 3-list of int or float defining a sampling interval
-        elif isinstance(value, list):
-            if expected_type not in [float, int]:
-                raise ValueError(
-                    f"list elements should be int or float, cannot define sampling interval from {type_name}"
-                )
-            for v in value:
-                if not isinstance(v, expected_type):
-                    raise ValueError(
-                        f"{arg_name} list expects elements of type {type_name}, received {type(v).__name__}"
-                    )
-            if len(value) not in [2, 3]:
-                raise ValueError(
-                    f"{arg_name} list should be [min, max] or [min, max, increment], received {value}"
-                )
-
-        else:
-            if expected_type in [float, int]:
-                raise ValueError(
-                    f"{arg_name} must be {type_name}, list[{type_name}], or set[{type_name}], received {type(value).__name__}"
-                )
-            else:
-                raise ValueError(
-                    f"{arg_name} must be {type_name} or set[{type_name}], received {type(value).__name__}"
-                )
-
-
-def set_hyperparameter(value):
-    """Converts hyperparameter arguments to ray.tune search spaces.
-
-    Recurses through nested dictionaries converting list and set values to ray.tune search
-    spaces. All other values are set as specific hyperparameter choices.
-
-    Parameters
-    ----------
-    value : dict, set, list, or other
-        A nested dictionary of hyperparameters, containing list, set, and other entries.
-    """
-
-    # user provides set, list, or specific value like int, float, str, or dict
-    if isinstance(value, set):
-        output = tune.choice(list(value))
-
-    elif isinstance(value, list):
-        # determine increment
-        if len(value) == 2:
-            if isinstance(value[0], int):
-                inc = 1
-            elif isinstance(value[0], float):
-                inc = (value[1] - value[0]) / 10.0
-        elif len(value) == 3:
-            inc = value[2]
-        else:
-            raise ValueError(
-                f"value argument list should be [min, max] or [min, max, increment], received {value}"
-            )
-
-        # set min, max, and increment depending on type
-        if isinstance(value[0], int):
-            output = tune.qrandint(value[0], value[1], inc)
-        elif isinstance(value[0], float):
-            output = tune.quniform(value[0], value[1], inc)
-
-    elif isinstance(value, dict):
-        output = {name: set_hyperparameter(value[name]) for name in value.keys()}
-
-    else:
-        output = value
-
-    return output
+    if isinstance(space, dict):
+        config = {}
+        for key in space:
+            config[key] = sample_space(space[key])
+        return config
+    elif isinstance(space, (sample.Categorical, sample.Integer, sample.Float)):
+        return sample_space(space.sample())
+    else:  # non sampleable or nestable value
+        return space
