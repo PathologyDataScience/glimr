c0e1c302c28f24d1dbd67d0c698bf2d09442fd89
Lee Cooper <lee.cooper@northwestern.edu>
2023-04-08 09:46:25 -0500

    Fix docstring

--- glimr/optimization.py
+++ glimr/optimization.py
@@ -11,37 +11,38 @@ def optimization_space(
     beta_2=tune.quniform(0.5, 1.0, 1e-2),
 ):
     """Generate a search space for a training and optimization parameters.
-    This function is used to generate a search space for batching and gradient optimization
-    parameters. This includes batch size, gradient optimization method, and method
-    parameters like learning rate. The search space for each parameter
-    is defined as: 1. A set defining specific values to search 2. A list specifying a search
-    range [min, max, increment(optional)] 3. A specific value to assign to hyperparameter
-    (no search).
+    
+    This function creates a search space for gradient optimization parameters
+    including gradient optimization method, and method hyperparameters like
+    learning rate or momentum. The search space for each hyperparameter is
+    defined using the ray tune search space API.
+    
     Parameters
     ----------
     epochs : int
-        The maximum number of epochs to train a model for. This can be overrided by the
-        scheduler or by the `stopper` attribute of a `Search` object.
-    method : set[string]
-        A list of strings encoding the gradient optimization method. The strings in `method`
-        are converted to tf.keras.optimizer objects during training by
-        search.utils.keras_optimizer. Default value is {"rms", "sgd", "adadelta", "adagrad",
-        "adam"}.
-    learning_rate : list[float]
-        The range of learning rates for the gradient optimizer. Default value
-        is [1e-5, 1e-2, 1e-5].
+        The maximum number of epochs to train a model for. Can be overrided 
+        by the scheduler or  the `stopper` attribute of a `Search` object.
+    method : str, ray.tune.search.sample.Categorical
+        A string or categorical search parameter defining the gradient 
+        optimization method. Strings are converted to tf.keras.optimizer objects 
+        during training by search.utils.keras_optimizer. Default value is 
+        tune.choice(["rms", "sgd", "adadelta", "adagrad", "adam"]).
+    learning_rate : float, ray.tune.search.sample.Float
+        The range of learning rates for the gradient optimizer. Default value 
+        is tune.quniform(1e-5, 1e-2, 1e-5).
     rho : list[float]
         The range of learning rate decay values for the optimizers. Default value
-        is [0.5, 1.0, 1e-2].
-    momentum : list[float]
+        is tune.quniform(0.5, 1.0, 1e-2).
+    momentum : float, ray.tune.search.sample.Float
         The range of momentum values for the momentum optimizers. Default value
-        is [0.0, 1e-1, 1e-2].
-    beta_1 : list[float]
+        is tune.quniform(0.0, 1e-1, 1e-2).
+    beta_1 : float, ray.tune.search.sample.Float
         The range of beta_1 values for the adam optimizer. Default value is
-        [0.5, 1.0, 1e-2].
-    beta_2 : list[float]
+        tune.quniform(0.5, 1.0, 1e-2).
+    beta_2 : float, ray.tune.search.sample.Float
         The range of beta_2 values for the adam optimizer. Default value is
-        [0.5, 1.0, 1e-2].
+        tune.quniform(0.5, 1.0, 1e-2).
+
     Returns
     -------
     task : dict
